afe0eaa0570ceb4201f5c4cea9ca908d11db50a4
Francesco Banconi <francesco.banconi@canonical.com>
2021-06-06 02:08:56 +0200

    wip

--- strategy/strategy.go
+++ strategy/strategy.go
@@ -33,7 +33,6 @@ func Move(state *params.GameRequest) Direction {
 		fmt.Println("  one choice only")
 		return ds[0]
 	}
-	fmt.Printf("possible directions: %#v\n", ds)
 
 	// Then refine the selection.
 	rand.Shuffle(len(ds), func(i, j int) { ds[i], ds[j] = ds[j], ds[i] })
